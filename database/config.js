840f824bebe11b5bacabe899d8cfa2e1e52a116f
alesteantcer <83327917+alesteantcer@users.noreply.github.com>
2022-04-09 02:43:19 -0500

    Update config.js

--- database/config.js
+++ database/config.js
@@ -10,7 +10,10 @@ const dbConnection = async () => {
 		await mongoose.connect(process.env.DB_MONGO, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-		});
+		}); /* en la version 6 ya no va , {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}*/
 		console.log("DB Conectado");
 	} catch (error) {
 		console.log("hubo un error");
